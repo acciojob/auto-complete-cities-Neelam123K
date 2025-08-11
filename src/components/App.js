64700aea29134f9e2332a827df063d55a150fdd5
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-08-11 23:02:41 +0530

    data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
-import AutoCompleteCities from "./AutocompleteCities";
+import AutocompleteCities from "./AutocompleteCities";
 
+const citySuggestions = [
+  "Bangalore",
+  "Mumbai",
+  "Delhi",
+  "Kolkata",
+  "Chennai",
+  "Hyderabad",
+  "Pune",
+  "Ahmedabad",
+  "Jaipur",
+  "Surat",
+];
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <AutoCompleteCities/>
+        <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h3>Search cities of India:</h3>
+      <AutocompleteCities suggestions={citySuggestions} />
+    </div>
     </div>
   )
 }
